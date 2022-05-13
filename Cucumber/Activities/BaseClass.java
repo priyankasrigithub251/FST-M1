15cb9246d94367dc1ac169ac08919ccb1197083d
priyankasrigithub251 <104206581+priyankasrigithub251@users.noreply.github.com>
2022-05-13 13:12:37 +0530

    Add files via upload

---
+++ Cucumber/Activities/BaseClass.java
@@ -0,0 +1,14 @@
+package StepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+   static WebDriver driver= new ChromeDriver();
+   static WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
+}
